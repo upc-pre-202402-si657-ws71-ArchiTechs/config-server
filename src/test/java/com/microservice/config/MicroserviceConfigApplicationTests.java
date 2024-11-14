d25d875f53a99cec7247696ea6e1c683a752dfdf
GrandProgTest <u202124343@upc.edu.pe>
2024-11-14 16:18:27 -0500

    initial commit

---
+++ src/test/java/com/microservice/config/MicroserviceConfigApplicationTests.java
@@ -0,0 +1,14 @@
+package com.microservice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+class MicroserviceConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
